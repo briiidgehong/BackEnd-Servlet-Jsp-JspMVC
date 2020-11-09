f67c6b8cd1a33a65e9f3db654d89ed5691a0a5b4
cheepiz92 <73451727+mangozzelli@users.noreply.github.com>
2020-11-09 13:19:09 +0900

    브라우저에게 text를 보내는 형식지정(text/html, charset=UTF-8)

--- src/mangozzelli/ServletAnnotation.java
+++ src/mangozzelli/ServletAnnotation.java
@@ -13,10 +13,19 @@ public class ServletAnnotation extends HttpServlet {
     public void service(HttpServletRequest request,
                         HttpServletResponse response) throws IOException, ServletException {
 
+        //우리는 리스폰스 텍스트의 인코딩 형식을 UTF-8 로 사용할꺼야
+        response.setCharacterEncoding("UTF-8");
+        //브라우저에게 명시 : 우리가 보내는 텍스트는 html 이고, 인코딩 형식은 UTF-8이야
+        response.setContentType("text/html; charset=UTF-8");
+
         System.out.println("hello Servlet !!! using console");
 
         PrintWriter writer = response.getWriter();
-        writer.println("hello servlet !!! using printwriter and annotation");
+
+        for(int i=0;i<10;i++)
+        {
+            writer.println(i + " 안녕 servlet<br>" );
+        }
 
 
     }
