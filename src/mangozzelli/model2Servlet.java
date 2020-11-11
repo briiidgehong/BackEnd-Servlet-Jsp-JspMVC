8ed3ffbd69bba9789f0044ab42dc5b98ae71374f
cheepiz92 <73451727+mangozzelli@users.noreply.github.com>
2020-11-11 13:24:51 +0900

    MVC Model 2

---
+++ src/mangozzelli/model2Servlet.java
@@ -0,0 +1,44 @@
+package mangozzelli;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/model2Servlet")
+public class model2Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Model 2를 구현하기 위해 Controller 부분을 jsp 에서 Servlet 으로 분기
+        // controller : servlet -> model -> view : jsp
+
+        //입력과 제어 Controller
+        int intNum = 0;
+        String strNum = "0";
+        strNum = request.getParameter("n");
+        if(strNum != null && !strNum.equals(""))
+            intNum = Integer.parseInt(strNum);
+
+        String model ="";
+        if(intNum%2 !=0){
+            model = "홀수";
+        }else {
+            model = "짝수";
+        }
+
+        // 출력 데이터 Model
+        // 데이터 전달을 위해 forwarding(운송) 사용
+        // -> dispatcher(지상에서 근무하는 항공기 운항 관리자) 사용
+        // 서블릿간(servlet - servlet(=jsp 또한 포함)) 데이터 공유는 requst 객체를 사용한다.
+        request.setAttribute("model",model);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("spagettiJSP.jsp");
+        // 여기 서블릿에서 쓰고있는 request 와 response 객체를 그대로 jsp 페이지로 보낸다.
+        // 여기 서블릿의 requst, response 객체와 jsp page의 request response 가 같아져서 데이터를 공유하는 효과
+        requestDispatcher.forward(request,response);
+
+        // 이렇게 이어가는 것이 아니라 새로 요청 할때는 redirect 쓰면 된다.
+    }
+}
--- web/spagettiJSP.jsp
+++ web/spagettiJSP.jsp
@@ -1,24 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java"
     pageEncoding="UTF-8" %>
-<!-- MVC Model 1 -->
-<!-- 스파게티처럼 꼬이지 않게 제어단과 뷰단을 나누자 -->
-
-<!-- 입력과 제어 Controller-->
-<%
-    int intNum = 0;
-    String strNum = "0";
-    strNum = request.getParameter("n");
-    if(strNum != null && !strNum.equals(""))
-        intNum = Integer.parseInt(strNum);
-
-    String model ="";
-    if(intNum%2 !=0){
-        model = "홀수";
-    }else {
-        model = "짝수";
-    }
-%>
-<!-- 출력 데이터 Model-->
 
 <!-- 출력 담당 View -->
 <html>
@@ -26,6 +7,6 @@
     <title>Title</title>
 </head>
 <body>
-    <%=model%> 입니다.
+    <%=request.getAttribute("model")%> 입니다.
 </body>
 </html>
