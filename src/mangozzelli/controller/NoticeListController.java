75786760510d1fc809e08969f50ad178efa6b4e5
cheepiz92 <73451727+mangozzelli@users.noreply.github.com>
2020-11-15 16:22:23 +0900

    noticeListController 에서 getNoticeList()을 service로 분리

--- src/mangozzelli/controller/NoticeListController.java
+++ src/mangozzelli/controller/NoticeListController.java
@@ -1,6 +1,7 @@
 package mangozzelli.controller;
 
 import mangozzelli.entity.Notice;
+import mangozzelli.service.NoticeService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,51 +19,14 @@ public class NoticeListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<Notice> list = new ArrayList<>();
+        NoticeService service = new NoticeService();
+        List<Notice> list = service.getNoticeList();
 
-        String url = "jdbc:oracle:thin:@localhost:1521/xe";
-        String sql = "SELECT * FROM NOTICE";
 
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = DriverManager.getConnection(url, "C##MANGOZZELLI", "0000");
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(sql);
 
-            while (rs.next()){
-                int id = rs.getInt("ID");
-                String title = rs.getString("TITLE");
-                String content = rs.getString("CONTENT");
-                Date regdate = rs.getDate("REGDATE");
-                String writer_id = rs.getString("WRITER_ID");
-                String hit = rs.getString("HIT");
-                String files = rs.getString("FILES");
+        request.setAttribute("list",list);
 
-                Notice notice = new Notice(
-                        id,
-                        title,
-                        content,
-                        regdate,
-                        writer_id,
-                        hit,
-                        files
-                );
-
-                list.add(notice);
-            }
-
-            request.setAttribute("list",list);
-
-            request.getRequestDispatcher("/WEB-INF/view/notice/list.jsp")
-                    .forward(request,response);
-
-            rs.close();
-            st.close();
-            con.close();
-
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("ODBC CLASS LOAD 실패");
-            e.printStackTrace();
-        }
+        request.getRequestDispatcher("/WEB-INF/view/notice/list.jsp")
+                .forward(request,response);
     }
 }
